d01382e286b385facfc4dd64ab36db5fc9a49ec8
wpk <william.krekelberg@nist.gov>
2024-01-19 15:50:51 -0500

    chore: update cruft

--- docs/conf.py
+++ docs/conf.py
@@ -241,24 +241,12 @@ project = "cmomy"
 copyright = "2020, William P. Krekelberg"  # noqa: A001
 author = "William P. Krekelberg"
 
+
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
 # the built documents.
 #
 # The short X.Y version.
-# versioning with scm with editable install has issues.
-# instead, try to use scm if available.
-# try:
-#     from setuptools_scm import get_version
-
-#     version = get_version(root="..", relative_to=__file__)
-#     release = version
-# except ImportError:
-#     version = cmomy.__version__
-#     # The full version, including alpha/beta/rc tags.
-#     release = cmomy.__version__
-
-
 def _get_version() -> str:
     if (version := os.environ.get("SETUPTOOLS_SCM_PRETEND_VERSION")) is None:
         version = cmomy.__version__
@@ -345,7 +333,7 @@ def get_ipython_savefig_dir() -> str:
     d = Path(__file__).parent / "_build" / "html" / "_static"
     if not d.is_dir():
         d.mkdir(parents=True)
-    return d
+    return str(d)
 
 
 ipython_savefig_dir = get_ipython_savefig_dir()
