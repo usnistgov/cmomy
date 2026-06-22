2631408eb716d0acf934d778b260d7ef6f8b6afe
wpk-nist-gov <wpk@nist.gov>
2026-06-22 10:04:39 -0400

    chore(template): accept new copier update (#185)
    Co-authored-by: wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True)  # noqa: RUF076
 def add_standard_imports(doctest_namespace) -> None:
     import numpy as np
     import pandas as pd
