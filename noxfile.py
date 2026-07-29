785e5dc322687aa9e651f000a2206610cad4afe3
wpk-nist-gov <wpk@nist.gov>
2026-07-28 23:01:33 -0400

    chore(template): accept new copier update (#219)
    Co-authored-by: wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -888,7 +888,8 @@ def typecheck(  # ruff:ignore[too-many-branches]
             # "pyrefly",
             # "ty",
             "pylint",
-            "typecheck-notebook",
+            "mypy-notebook",
+            "pylint-notebook",
         ]
 
     # set the cache directory for mypy
