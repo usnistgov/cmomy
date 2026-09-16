1dbf1d94d6c8f90e4eae539f6d84406b2f92ea51
wpk-nist-gov <wpk@nist.gov>
2026-09-16 14:21:05 -0400

    chore: fix conflicts

--- tools/auto_test_typing.py
+++ tools/auto_test_typing.py
@@ -371,7 +371,7 @@ for funcs, params in (
 
 
 # * Move axis
-params_moveaxis = list(filter(lambda x: "dataset" not in x[0] and "arraylike" not in x[0] and x[1] is None, params_genarraylike_to_genarray_dtype))  # pylint: disable=bad-builtin
+params_moveaxis = list(filter(lambda x: "dataset" not in x[0] and "arraylike" not in x[0] and x[1] is None, params_genarraylike_to_genarray_dtype))  # pylint: disable=bad-builtin  # pyrefly: ignore [implicit-any-lambda]
 funcs_moveaxis = [
     ("cmomy.moveaxis", "data_", None, "0, 1"),
 ]
@@ -554,14 +554,14 @@ out.extend(get_list(funcs_class_method_dtype, params_class_method_dtype, method=
 out.extend(get_list(funcs_class_method_dtype_out, params_class_method_dtype_out, method=True))
 
 # *** moveaxis
-params_class_method_moveaxis = list(filter(lambda x: "dataset" not in x[0], params_class_method))  # pylint: disable=bad-builtin
+params_class_method_moveaxis = list(filter(lambda x: "dataset" not in x[0], params_class_method))  # pylint: disable=bad-builtin  # pyrefly: ignore [implicit-any-lambda]
 funcs_class_method_moveaxis = [
     ("moveaxis", "central_", None, "0, 0"),
 ]
 out.extend(get_list(funcs_class_method_moveaxis, params_class_method_moveaxis, method=True))
 
 # *** reshape
-params_class_method_reshape = list(filter(lambda x: "data" not in x[0], params_class_method))  # pylint: disable=bad-builtin
+params_class_method_reshape = list(filter(lambda x: "data" not in x[0], params_class_method))  # pylint: disable=bad-builtin  # pyrefly: ignore [implicit-any-lambda]
 funcs_class_method_reshape = [
     ("reshape", "central_", None, "(2, 5)")
 ]
@@ -607,9 +607,9 @@ params_class_methodtoarray_dtype_out = [
     ("arraylike", "float64", "float32", "NDArray[float32]", "float32"),
     ("dataarray", None, "float32", "xr.DataArray", "float32", "xr.DataArray"),
 ]
-funcs_class_methodtoarray = [
+funcs_class_methodtoarray = [  # pyrefly: ignore [implicit-any-empty-container]
 ]
-funcs_class_methodtoarray_dtype = [
+funcs_class_methodtoarray_dtype = [  # pyrefly: ignore [implicit-any-empty-container]
 ]
 funcs_class_methodtoarray_dtype_out = [
     ("cumulative", "central_", "transform_out_", "{axis_dim}")
